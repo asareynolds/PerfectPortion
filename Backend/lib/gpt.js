448409b2437be1fea68f59f8aa802b933311770c
Aiden Johnson <aiden@tgmstudios.net>
2023-10-22 17:06:41 +0000

    Fix Prompt

--- Backend/lib/gpt.js
+++ Backend/lib/gpt.js
@@ -20,7 +20,7 @@ let APIcall = async (prompt) => {
     });
 }
 
-const getRecommendations = async function(imageID, age, weight, gender,height) {
+const getRecommendations = async function(imageID, age, weight, gender,height,allergies) {
     const nutrients = JSON.parse(fs.readFileSync("/usr/src/usr_images/" + imageID + ".json", 'utf8'));
     var foodStrings = "";
 
@@ -28,7 +28,7 @@ const getRecommendations = async function(imageID, age, weight, gender,height) {
         food = nutrients[property];
         foodStrings += `${food.foodName} with a calorie count of ${Math.round(food.info.calories)} with ${Math.round(food.info.protein.quantity)}${food.info.protein.unit} of protein & ${Math.round(food.info.carb.quantity)}${food.info.carb.unit} of carbs & ${Math.round(food.info.fat.quantity)}${food.info.fat.unit} of fat, `;
     }
-    var prompt = `This is a meal for a ${age}-year-old ${gender} who weighs ${weight} lbs and is ${height}inches tall. Meal: ${foodStrings}. Evaluate this meal from a nutritional perspective. Do not mention the prompt in the answer, simply give a succinct response, 1 to 2 sentences per point, discussing: 1. Nutritional Value 2. Recommendations for a better meal, including specific foods, or no recommendation if the meal is sufficient.`;
+    var prompt = `This is a meal for a ${age}-year-old ${gender} who weighs ${weight} lbs and is ${height}inches tall. Meal: ${foodStrings}. Evaluate this meal from a nutritional perspective. Do not mention the prompt in the answer, simply give a succinct response, 1 to 2 sentences per point and never more than 2 sentences, discussing: 1. Nutritional Value 2. Recommendations for a better meal, including specific foods, or no recommendation if the meal is sufficient.`;
     console.log("Sending request to ChatGPT with prompt: " + prompt);
     var response = await APIcall(prompt);
     return response['content']
